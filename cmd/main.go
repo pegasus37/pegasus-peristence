67b60e7dff447df5ea4b26e712e8886c2356ad45
Fadrian <fadrian.merdhianto@ruma.co.id>
2019-05-05 16:23:02 +0700

    add dep

--- cmd/main.go
+++ cmd/main.go
@@ -1,49 +1,42 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-
-	"github.com/gorilla/mux"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
-	"github.com/spf13/viper"
 )
 
 func main() {
-	router := mux.NewRouter()
+	// router := mux.NewRouter()
 
-	setUpEnv()
-	setUpConfig()
-	setUpRouter(router)
+	// setUpEnv()
+	// setUpConfig()
+	// setUpRouter(router)
 
-	startServer(router)
+	// startServer(router)
 }
 
-func setUpEnv() {
-	if os.Getenv("env") == "" {
-		os.Setenv("env", "local")
-	}
-}
+// func setUpEnv() {
+// 	if os.Getenv("env") == "" {
+// 		os.Setenv("env", "local")
+// 	}
+// }
 
-func setUpConfig() {
-	viper.SetConfigType("toml")
-	viper.SetConfigName("config")
-	viper.AddConfigPath("./../config")
+// func setUpConfig() {
+// 	viper.SetConfigType("toml")
+// 	viper.SetConfigName("config")
+// 	viper.AddConfigPath("./../config")
 
-	err := viper.ReadInConfig()
-	if err != nil {
-		log.Fatalf("Fatal error config file: %s \n", err)
-	}
-}
+// 	err := viper.ReadInConfig()
+// 	if err != nil {
+// 		log.Fatalf("Fatal error config file: %s \n", err)
+// 	}
+// }
 
-func startServer(router *mux.Router) {
-	fmt.Println(viper.GetString("app.name") + " is running on port: " + viper.GetString("app.port"))
-	fmt.Println("env: " + os.Getenv("env"))
+// func startServer(router *mux.Router) {
+// 	fmt.Println(viper.GetString("app.name") + " is running on port: " + viper.GetString("app.port"))
+// 	fmt.Println("env: " + os.Getenv("env"))
 
-	err := http.ListenAndServe(":"+viper.GetString("app.port"), router)
-	if err != nil {
-		log.Fatal("ListenAndServe Error:", err)
-	}
-}
+// 	err := http.ListenAndServe(":"+viper.GetString("app.port"), router)
+// 	if err != nil {
+// 		log.Fatal("ListenAndServe Error:", err)
+// 	}
+// }
--- cmd/router.go
+++ cmd/router.go
@@ -1,14 +1,10 @@
 package main
 
-import (
-	"github.com/gorilla/mux"
-)
-
 const (
 	studentList = "/api/v1/student/"
 )
 
-func setUpRouter(router *mux.Router) {
-	router.HandleFunc(studentList, handler.GetStudentList).Methods("GET")
-	router.HandleFunc(studentList, handler.PostStudentList).Methods("POST")
-}
+// func setUpRouter(router *mux.Router) {
+// 	router.HandleFunc(studentList, handler.GetStudentList).Methods("GET")
+// 	router.HandleFunc(studentList, handler.PostStudentList).Methods("POST")
+// }
